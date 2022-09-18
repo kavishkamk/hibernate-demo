958ed5b00eff7b752c4751f80ad688bba64376e9
kavishkamk <kavishkamadhushan727@gmail.com>
2022-09-18 21:00:09 +0530

    configure hibernate with java 8 and Add to data to db

--- hibernate-demo/src/io/github/kavishkamk/jdbc/demo/entity/Student.java
+++ hibernate-demo/src/io/github/kavishkamk/jdbc/demo/entity/Student.java
@@ -8,22 +8,22 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "student")
 public class Student {
-	
+
 	@Id
 	@Column(name = "id")
 	private int id;
-	
+
 	@Column(name = "first_name")
 	private String firstName;
-	
+
 	@Column(name = "last_name")
 	private String lastName;
-	
+
 	@Column(name = "email")
 	private String email;
-	
+
 	public Student() {
-		
+
 	}
 
 	public Student(String firstName, String lastName, String email) {
